b15574c5f191071ddc494e4a6c07f96f90385dee
shengjiex98 <shengjiex98@@gmail.com>
2025-01-31 16:23:57 -0500

    I think this is the correct implementation for the multiplicative error.

--- src/control_guided_nas/get_max_diam.jl
+++ src/control_guided_nas/get_max_diam.jl
@@ -26,13 +26,11 @@ function get_max_diam(sys::StateSpace, latency_ms::Integer, errors::AbstractVect
     # regions to the control input dimensions. The error bound is calculated
     # by taking the maximum absolute value of each state dimension, and 
     # multiplying the error rate with it.
-    Wc(k, x) = let
-        x = reduce_order(x, 2 * sys.nx)
-        @info k x
-        # Find vertices of the zonotope. Each col is a vertex.
-        vertices_matrix = vertices_list(x) |> stack
-        # Find the maximum absolute value for each state dimension
-        max_states = maximum(abs.(vertices_matrix[1:sys.nx,:]), dims=2) |> vec
+    Wc(k::Integer, x::Zonotope) = let
+        # Use the generaotr matrix of the zonotope to calculate the maximum states
+        max_states = sum(abs.(x.generators[1:sys.nx,:]), dims=2) |> vec
+        @info k x.generators 
+        @info max_states
         Zonotope(
             zeros(sys.nx + sys.nu), 
             Diagonal([zeros(sys.nx); K * (errors .* max_states)])
