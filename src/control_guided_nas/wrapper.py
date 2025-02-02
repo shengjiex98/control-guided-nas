3eb6e3f7c16247a0460ac8d8a82b44a54a7c781f
shengjiex98 <shengjiex98@@gmail.com>
2025-02-02 16:06:20 -0500

    Only return the diameter result (not the full flowpipe)

--- src/control_guided_nas/wrapper.py
+++ src/control_guided_nas/wrapper.py
@@ -5,10 +5,10 @@ import pathlib
 
 jl.include(str(pathlib.Path(__file__).parent.resolve()) + "/get_max_diam.jl")
 
-def get_max_diam(latency: float, errors: float|list[float], sys: str="F1"):
-    sys = jl.seval(f"benchmarks[:{sys}]")
+def get_max_diam(latency: float, errors: float|list[float], s: str="F1"):
+    s = jl.seval(f"benchmarks[:{s}]")
     if isinstance(errors, float):
-        errors = [errors] * sys.nx
-    x0center = np.asarray([10.] * sys.nx)
-    x0size = np.asarray([1.] * sys.nx)
-    return jl.get_max_diam(sys, int(latency * 1000), np.asarray(errors), x0center, x0size)
+        errors = [errors] * s.nx
+    x0center = np.asarray([10.] * s.nx)
+    x0size = np.asarray([1.] * s.nx)
+    return jl.get_max_diam(s, int(latency * 1000), np.asarray(errors), x0center, x0size)[1]
